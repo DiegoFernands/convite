cda515401910562012ed093f413c479c08ae73de
diego <diego.fernandesjs@gmail.com>
2023-06-11 20:37:05 -0300

    ajuste audio

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 var audio = document.getElementById("audio");
 audio.currentTime = 10;
+function handleVisibilityChange() {
+    if (document.hidden) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+}
+
+document.addEventListener("visibilitychange", handleVisibilityChange, false);
